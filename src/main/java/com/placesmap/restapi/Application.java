8feb962fccf886c11c7a30caac8c19a89f5019f0
Fatih Yarlıgan <fatih-yarligan@outlook.com>
2024-04-26 18:22:08 +0300

    build & deploy for tomcat and CORS issue

--- src/main/java/com/placesmap/restapi/Application.java
+++ src/main/java/com/placesmap/restapi/Application.java
@@ -2,9 +2,24 @@ package com.placesmap.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+	
+    @RequestMapping("/")
+    String running() {
+        return "restAPI is running!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/placesmap/restapi/controller/PlaceController.java
+++ src/main/java/com/placesmap/restapi/controller/PlaceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import com.placesmap.restapi.model.SearchedArea;
 import com.placesmap.restapi.repository.SearchedAreaRepository;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/place")
 public class PlaceController {
